4fa8a0a1c3f5904dc51cd349d6448ee409a1c71f
= <elnadav12@gmail.com>
2025-01-15 00:37:50 +0200

    Extract _wait_for_ready_callbacks - passing tests

--- rclpy/rclpy/executors.py
+++ rclpy/rclpy/executors.py
@@ -566,6 +566,181 @@ class Executor(ContextManager['Executor']):
         """
         return not entity._executor_event and entity.callback_group.can_execute(entity)
 
+    
+    def _construct_wait_set_and_wait(self, nodes_to_use: list['Node'], timeout_timer: Optional[Timer], timeout_nsec: int):
+        # Gather entities that can be waited on
+        subscriptions: List[Subscription] = []
+        guards: List[GuardCondition] = []
+        timers: List[Timer] = []
+        clients: List[Client] = []
+        services: List[Service] = []
+        waitables: List[Waitable[Any]] = []
+        for node in nodes_to_use:
+            subscriptions.extend(filter(self.can_execute, node.subscriptions))
+            timers.extend(filter(self.can_execute, node.timers))
+            clients.extend(filter(self.can_execute, node.clients))
+            services.extend(filter(self.can_execute, node.services))
+            node_guards = filter(self.can_execute, node.guards)
+            waitables.extend(filter(self.can_execute, node.waitables))
+            # retrigger a guard condition that was triggered but not handled
+            for gc in node_guards:
+                if gc._executor_triggered:
+                    gc.trigger()
+                guards.append(gc)
+        if timeout_timer is not None:
+            timers.append(timeout_timer)
+
+        guards.append(self._guard)
+        guards.append(self._sigint_gc)
+
+        entity_count = NumberOfEntities(
+            len(subscriptions), len(guards), len(timers), len(clients), len(services))
+
+        # Construct a wait set
+        wait_set = None
+        with ExitStack() as context_stack:
+            sub_handles = []
+            for sub in subscriptions:
+                try:
+                    context_stack.enter_context(sub.handle)
+                    sub_handles.append(sub.handle)
+                except InvalidHandle:
+                    entity_count.num_subscriptions -= 1
+
+            client_handles = []
+            for cli in clients:
+                try:
+                    context_stack.enter_context(cli.handle)
+                    client_handles.append(cli.handle)
+                except InvalidHandle:
+                    entity_count.num_clients -= 1
+
+            service_handles = []
+            for srv in services:
+                try:
+                    context_stack.enter_context(srv.handle)
+                    service_handles.append(srv.handle)
+                except InvalidHandle:
+                    entity_count.num_services -= 1
+
+            timer_handles = []
+            for tmr in timers:
+                try:
+                    context_stack.enter_context(tmr.handle)
+                    timer_handles.append(tmr.handle)
+                except InvalidHandle:
+                    entity_count.num_timers -= 1
+
+            guard_handles = []
+            for gc in guards:
+                try:
+                    context_stack.enter_context(gc.handle)
+                    guard_handles.append(gc.handle)
+                except InvalidHandle:
+                    entity_count.num_guard_conditions -= 1
+
+            for waitable in waitables:
+                try:
+                    context_stack.enter_context(waitable)
+                    entity_count += waitable.get_num_entities()
+                except InvalidHandle:
+                    pass
+
+            context_stack.enter_context(self._context.handle)
+
+            wait_set = _rclpy.WaitSet(
+                entity_count.num_subscriptions,
+                entity_count.num_guard_conditions,
+                entity_count.num_timers,
+                entity_count.num_clients,
+                entity_count.num_services,
+                entity_count.num_events,
+                self._context.handle)
+
+            wait_set.clear_entities()
+            for sub_handle in sub_handles:
+                wait_set.add_subscription(sub_handle)
+            for cli_handle in client_handles:
+                wait_set.add_client(cli_handle)
+            for srv_capsule in service_handles:
+                wait_set.add_service(srv_capsule)
+            for tmr_handle in timer_handles:
+                wait_set.add_timer(tmr_handle)
+            for gc_handle in guard_handles:
+                wait_set.add_guard_condition(gc_handle)
+            for waitable in waitables:
+                waitable.add_to_wait_set(wait_set)
+
+            # Wait for something to become ready
+            wait_set.wait(timeout_nsec)
+            if self._is_shutdown:
+                raise ShutdownException()
+            if not self._context.ok():
+                raise ExternalShutdownException()
+
+            # get ready entities
+            subs_ready = wait_set.get_ready_entities('subscription')
+            guards_ready = wait_set.get_ready_entities('guard_condition')
+            timers_ready = wait_set.get_ready_entities('timer')
+            clients_ready = wait_set.get_ready_entities('client')
+            services_ready = wait_set.get_ready_entities('service')
+
+            # Mark all guards as triggered before yielding since they're auto-taken
+            for gc in guards:
+                if gc.handle.pointer in guards_ready:
+                    gc._executor_triggered = True
+
+            # Check waitables before wait set is destroyed
+            for node in nodes_to_use:
+                for wt in node.waitables:
+                    # Only check waitables that were added to the wait set
+                    if wt in waitables and wt.is_ready(wait_set):
+                        if wt.callback_group.can_execute(wt):
+                            handler = self._make_handler(wt, node, self._take_waitable)
+                            yielded_work = True
+                            yield handler, wt, node
+
+        # Process ready entities one node at a time
+        for node in nodes_to_use:
+            for tmr in node.timers:
+                if tmr.handle.pointer in timers_ready:
+                    # Check timer is ready to workaround rcl issue with cancelled timers
+                    if tmr.handle.is_timer_ready():
+                        if tmr.callback_group.can_execute(tmr):
+                            handler = self._make_handler(tmr, node, self._take_timer)
+                            yielded_work = True
+                            yield handler, tmr, node
+
+            for sub in node.subscriptions:
+                if sub.handle.pointer in subs_ready:
+                    if sub.callback_group.can_execute(sub):
+                        handler = self._make_handler(sub, node, self._take_subscription)
+                        yielded_work = True
+                        yield handler, sub, node
+
+            for gc in node.guards:
+                if gc._executor_triggered:
+                    if gc.callback_group.can_execute(gc):
+                        handler = self._make_handler(gc, node, self._take_guard_condition)
+                        yielded_work = True
+                        yield handler, gc, node
+
+            for client in node.clients:
+                if client.handle.pointer in clients_ready:
+                    if client.callback_group.can_execute(client):
+                        handler = self._make_handler(client, node, self._take_client)
+                        yielded_work = True
+                        yield handler, client, node
+
+            for srv in node.services:
+                if srv.handle.pointer in services_ready:
+                    if srv.callback_group.can_execute(srv):
+                        handler = self._make_handler(srv, node, self._take_service)
+                        yielded_work = True
+                        yield handler, srv, node
+
+        return timers_ready
+    
     def _wait_for_ready_callbacks(
         self,
         timeout_sec: Optional[Union[float, TimeoutObject]] = None,
@@ -611,176 +786,7 @@ class Executor(ContextManager['Executor']):
                     # Get rid of any tasks that are done
                     self._tasks = list(filter(lambda t_e_n: not t_e_n[0].done(), self._tasks))
 
-            # Gather entities that can be waited on
-            subscriptions: List[Subscription] = []
-            guards: List[GuardCondition] = []
-            timers: List[Timer] = []
-            clients: List[Client] = []
-            services: List[Service] = []
-            waitables: List[Waitable[Any]] = []
-            for node in nodes_to_use:
-                subscriptions.extend(filter(self.can_execute, node.subscriptions))
-                timers.extend(filter(self.can_execute, node.timers))
-                clients.extend(filter(self.can_execute, node.clients))
-                services.extend(filter(self.can_execute, node.services))
-                node_guards = filter(self.can_execute, node.guards)
-                waitables.extend(filter(self.can_execute, node.waitables))
-                # retrigger a guard condition that was triggered but not handled
-                for gc in node_guards:
-                    if gc._executor_triggered:
-                        gc.trigger()
-                    guards.append(gc)
-            if timeout_timer is not None:
-                timers.append(timeout_timer)
-
-            guards.append(self._guard)
-            guards.append(self._sigint_gc)
-
-            entity_count = NumberOfEntities(
-                len(subscriptions), len(guards), len(timers), len(clients), len(services))
-
-            # Construct a wait set
-            wait_set = None
-            with ExitStack() as context_stack:
-                sub_handles = []
-                for sub in subscriptions:
-                    try:
-                        context_stack.enter_context(sub.handle)
-                        sub_handles.append(sub.handle)
-                    except InvalidHandle:
-                        entity_count.num_subscriptions -= 1
-
-                client_handles = []
-                for cli in clients:
-                    try:
-                        context_stack.enter_context(cli.handle)
-                        client_handles.append(cli.handle)
-                    except InvalidHandle:
-                        entity_count.num_clients -= 1
-
-                service_handles = []
-                for srv in services:
-                    try:
-                        context_stack.enter_context(srv.handle)
-                        service_handles.append(srv.handle)
-                    except InvalidHandle:
-                        entity_count.num_services -= 1
-
-                timer_handles = []
-                for tmr in timers:
-                    try:
-                        context_stack.enter_context(tmr.handle)
-                        timer_handles.append(tmr.handle)
-                    except InvalidHandle:
-                        entity_count.num_timers -= 1
-
-                guard_handles = []
-                for gc in guards:
-                    try:
-                        context_stack.enter_context(gc.handle)
-                        guard_handles.append(gc.handle)
-                    except InvalidHandle:
-                        entity_count.num_guard_conditions -= 1
-
-                for waitable in waitables:
-                    try:
-                        context_stack.enter_context(waitable)
-                        entity_count += waitable.get_num_entities()
-                    except InvalidHandle:
-                        pass
-
-                context_stack.enter_context(self._context.handle)
-
-                wait_set = _rclpy.WaitSet(
-                    entity_count.num_subscriptions,
-                    entity_count.num_guard_conditions,
-                    entity_count.num_timers,
-                    entity_count.num_clients,
-                    entity_count.num_services,
-                    entity_count.num_events,
-                    self._context.handle)
-
-                wait_set.clear_entities()
-                for sub_handle in sub_handles:
-                    wait_set.add_subscription(sub_handle)
-                for cli_handle in client_handles:
-                    wait_set.add_client(cli_handle)
-                for srv_capsule in service_handles:
-                    wait_set.add_service(srv_capsule)
-                for tmr_handle in timer_handles:
-                    wait_set.add_timer(tmr_handle)
-                for gc_handle in guard_handles:
-                    wait_set.add_guard_condition(gc_handle)
-                for waitable in waitables:
-                    waitable.add_to_wait_set(wait_set)
-
-                # Wait for something to become ready
-                wait_set.wait(timeout_nsec)
-                if self._is_shutdown:
-                    raise ShutdownException()
-                if not self._context.ok():
-                    raise ExternalShutdownException()
-
-                # get ready entities
-                subs_ready = wait_set.get_ready_entities('subscription')
-                guards_ready = wait_set.get_ready_entities('guard_condition')
-                timers_ready = wait_set.get_ready_entities('timer')
-                clients_ready = wait_set.get_ready_entities('client')
-                services_ready = wait_set.get_ready_entities('service')
-
-                # Mark all guards as triggered before yielding since they're auto-taken
-                for gc in guards:
-                    if gc.handle.pointer in guards_ready:
-                        gc._executor_triggered = True
-
-                # Check waitables before wait set is destroyed
-                for node in nodes_to_use:
-                    for wt in node.waitables:
-                        # Only check waitables that were added to the wait set
-                        if wt in waitables and wt.is_ready(wait_set):
-                            if wt.callback_group.can_execute(wt):
-                                handler = self._make_handler(wt, node, self._take_waitable)
-                                yielded_work = True
-                                yield handler, wt, node
-
-            # Process ready entities one node at a time
-            for node in nodes_to_use:
-                for tmr in node.timers:
-                    if tmr.handle.pointer in timers_ready:
-                        # Check timer is ready to workaround rcl issue with cancelled timers
-                        if tmr.handle.is_timer_ready():
-                            if tmr.callback_group.can_execute(tmr):
-                                handler = self._make_handler(tmr, node, self._take_timer)
-                                yielded_work = True
-                                yield handler, tmr, node
-
-                for sub in node.subscriptions:
-                    if sub.handle.pointer in subs_ready:
-                        if sub.callback_group.can_execute(sub):
-                            handler = self._make_handler(sub, node, self._take_subscription)
-                            yielded_work = True
-                            yield handler, sub, node
-
-                for gc in node.guards:
-                    if gc._executor_triggered:
-                        if gc.callback_group.can_execute(gc):
-                            handler = self._make_handler(gc, node, self._take_guard_condition)
-                            yielded_work = True
-                            yield handler, gc, node
-
-                for client in node.clients:
-                    if client.handle.pointer in clients_ready:
-                        if client.callback_group.can_execute(client):
-                            handler = self._make_handler(client, node, self._take_client)
-                            yielded_work = True
-                            yield handler, client, node
-
-                for srv in node.services:
-                    if srv.handle.pointer in services_ready:
-                        if srv.callback_group.can_execute(srv):
-                            handler = self._make_handler(srv, node, self._take_service)
-                            yielded_work = True
-                            yield handler, srv, node
+            timers_ready = yield from self._construct_wait_set_and_wait(nodes_to_use, timeout_timer, timeout_nsec)
 
             # Check timeout timer
             if (
@@ -972,3 +978,92 @@ class MultiThreadedExecutor(Executor):
         success: bool = super().shutdown(timeout_sec)
         self._executor.shutdown(wait=wait_for_threads)
         return success
+
+# from asyncio import TaskGroup
+# import asyncio
+# from concurrent.futures import ThreadPoolExecutor
+
+# # if we must "spin once" it is possible to inherit from EventLoop and access _run_once()
+# class AsyncioExecutor(Executor):
+#     def __init__(self):
+#         super().__init__()
+#         self._loop = asyncio.new_event_loop()
+#         self._task_group = TaskGroup()
+#         self._thread_pool = ThreadPoolExecutor(max_workers=1)
+
+    
+#     def _wait_for_ready_callbacks(self)
+    
+#     async def _wait_for_ready_callbacks_in_thread(self):
+#         await self._loop.run_in_executor(self._thread_pool, self._construct_wait_set_and_wait)
+
+#     async def _spin(self):
+#         async with self._task_group:
+#             self._task_group.create_task(self._wait_for_ready_callbacks_in_thread())
+
+#     def spin(self):
+#         asyncio.run(self._spin())
+
+
+#     def _spin_once_impl(
+#         self,
+#         timeout_sec: Optional[Union[float, TimeoutObject]] = None,
+#         wait_condition: Callable[[], bool] = lambda: False
+#     ) -> None:
+#         try:
+#             handler, entity, node = self.wait_for_ready_callbacks(
+#                 timeout_sec, None, wait_condition)
+#         except ShutdownException:
+#             pass
+#         except TimeoutException:
+#             pass
+#         except ConditionReachedException:
+#             pass
+#         else:
+#             handler()
+#             if handler.exception() is not None:
+#                 raise handler.exception()
+
+#             handler.result()  # raise any exceptions
+
+
+#     def wait_for_ready_callbacks(self, *args, **kwargs) -> Tuple[Task, WaitableEntityType, 'Node']:
+#         """
+#         Return callbacks that are ready to be executed.
+
+#         The arguments to this function are passed to the internal method
+#         :meth:`_wait_for_ready_callbacks` to get a generator for ready callbacks:
+
+#         .. Including the docstring for the hidden function for reference
+#         .. automethod:: _wait_for_ready_callbacks
+#         """
+#         while True:
+#             if self._cb_iter is None or self._last_args != args or self._last_kwargs != kwargs:
+#                 # Create a new generator
+#                 self._last_args = args
+#                 self._last_kwargs = kwargs
+#                 self._cb_iter = self._wait_for_ready_callbacks(*args, **kwargs)
+
+#             try:
+#                 return next(self._cb_iter)
+#             except StopIteration:
+#                 # Generator ran out of work
+#                 self._cb_iter = None
+
+#     def __enter__(self) -> 'Executor':
+#         # Nothing to do here
+#         return self
+
+#     def __exit__(
+#         self,
+#         exc_type: Optional[Type[BaseException]],
+#         exc_val: Optional[BaseException],
+#         exc_tb: Optional[TracebackType],
+#     ) -> None:
+#         self.shutdown()
+
+
+#     def spin(self) -> None:
+#         """Execute callbacks until shutdown."""
+#         while self._context.ok() and not self._is_shutdown:
+#             self.spin_once()
