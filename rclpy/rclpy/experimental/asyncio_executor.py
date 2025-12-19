854d70e48b4d556129b2e2eeaf6607694c0ea802
Nadav Elkabets <elnadav12@gmail.com>
2025-12-19 14:34:44 +0000

    Fix bug in remove_node
    Signed-off-by: Nadav Elkabets elnadav12@gmail.com

--- rclpy/rclpy/experimental/asyncio_executor.py
+++ rclpy/rclpy/experimental/asyncio_executor.py
@@ -14,48 +14,25 @@
 
 import asyncio
 from functools import partial
-from sys import stderr
-import time
 import traceback
-from typing import Any
 from typing import Callable
 from typing import Coroutine
 from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Set
-from typing import Type
 from typing import TypeVar
 from typing import Union
 
-from attr import dataclass
-
 from rclpy.client import Client
-from rclpy.clock import ClockChange, JumpHandle, JumpThreshold, ROSClock, TimeJump
-from rclpy.constants import S_TO_NS
 from rclpy.context import Context
-from rclpy.duration import Duration
-from rclpy.executors import await_or_execute
 from rclpy.executors import BaseExecutor
-from rclpy.executors import ExternalShutdownException
-from rclpy.executors import TracebackType
-from rclpy.logging import get_logger
 from rclpy.node import Node
 from rclpy.service import Service
 from rclpy.subscription import Subscription
-from rclpy.task import Future
-from rclpy.task import Task
-from rclpy.time import Time
-from rclpy.timer import Timer
 from rclpy.utilities import get_default_context
 
 EntityT = TypeVar('EntityT', bound=Union[Subscription, Service, Client])
-
-
-@dataclass
-class NodeEntities:
-    tasks: Set[Task] = set()
-    subscriptions: Set[Subscription] = set()
 
 
 class AsyncioExecutor(BaseExecutor):
@@ -67,11 +44,13 @@ class AsyncioExecutor(BaseExecutor):
         self._loop = loop or self._get_loop()
         self._context = context or get_default_context()
         # self._context.on_shutdown(self.shutdown)
-        self._node_to_entities: Dict[Node, NodeEntities] = {}
+        self._nodes: Set['Node'] = set()
+        self._subscription_to_node: Dict[Subscription, Node] = {}
+        self._node_to_tasks: Dict[Node, Set[asyncio.Task]] = {}
 
     def get_nodes(self) -> List['Node']:
         """Return nodes that have been added to this executor."""
-        return list(self._node_to_entities.keys())
+        return list(self._nodes)
 
     @property
     def context(self) -> Context:
@@ -82,18 +61,18 @@ class AsyncioExecutor(BaseExecutor):
     def loop(self) -> asyncio.AbstractEventLoop:
         return self._loop
 
-    def __enter__(self) -> 'AsyncioExecutor':
+    async def __enter__(self) -> 'AsyncioExecutor':
         return self
 
-    # def __exit__(
+    # async def __exit__(
     #     self,
     #     exc_type: Optional[Type[BaseException]],
     #     exc_val: Optional[BaseException],
     #     exc_tb: Optional[TracebackType],
     # ) -> None:
-    #     self.shutdown()
+    #     await self.shutdown()
 
-    # def shutdown(self, timeout_sec: Optional[float] = None) -> bool:
+    # async def shutdown(self, timeout_sec: Optional[float] = None) -> bool:
     #     """Clear all nodes and close the event loop."""
     #     if not self._shutdown_fut.done():
     #         self._shutdown_fut.set_result(None)
@@ -127,37 +106,39 @@ class AsyncioExecutor(BaseExecutor):
         self._update_entities_from_nodes()
 
     def add_node(self, node: Node) -> bool:
-        if node in self._node_to_entities:
+        if node in self._nodes:
             return False
 
-        self._node_to_entities[node] = NodeEntities()
+        self._nodes.add(node)
+        self._node_to_tasks[node] = set()
         node.executor = self
         self._update_entities_from_nodes()
         return True
 
     async def remove_node(self, node: Node) -> None:
-        if node not in self._node_to_entities:
+        if node not in self._nodes:
             return
 
+        self._nodes.remove(node)
         self._update_entities_from_nodes()
-        node_tasks = self._node_to_entities[node].tasks
+
+        node_tasks = self._node_to_tasks.pop(node)
         for task in node_tasks:
             task.cancel()
-        del self._node_to_entities[node]
-        await asyncio.gather(node_tasks)
+        if node_tasks:
+            await asyncio.gather(*node_tasks, return_exceptions=True)
 
     def _update_entities_from_nodes(self) -> None:
-        for node, node_entities in self._node_to_entities.items():
-            subscriptions = set()
-            subscriptions.update(node.subscriptions)
+        new_subscriptions: Dict[Subscription, Node] = {}
+        for node in self._nodes:
+            new_subscriptions.update({sub: node for sub in node.subscriptions})
 
-            self._update_entity_set(
-                node,
-                node_entities.subscriptions,
-                subscriptions,
-                self._handle_added_subscription,
-                self._handle_removed_subscription
-            )
+        self._update_entity_set(
+            self._subscription_to_node,
+            new_subscriptions,
+            self._handle_added_subscription,
+            self._handle_removed_subscription
+        )
     
     def _handle_added_subscription(self, sub: Subscription, node: Node):
         sub.handle.set_on_new_message_callback(
@@ -175,25 +156,28 @@ class AsyncioExecutor(BaseExecutor):
 
     def _update_entity_set(
         self,
-        node: Node,
-        current_set: set[EntityT],
-        new_set: set[EntityT],
-        new_entity_cb: Callable[[EntityT], None],
-        removed_entity_cb: Callable[[EntityT], None],
+        current_entity_to_node: Dict[EntityT, Node],
+        new_entity_to_node: Dict[EntityT, Node],
+        on_added_entity: Callable[[EntityT, Node], None],
+        on_removed_entity: Callable[[EntityT], None],
     ) -> bool:
-        added_entities = new_set - current_set
+        current_entities = set(current_entity_to_node.keys())
+        new_entities = set(new_entity_to_node.keys())
+
+        added_entities = new_entities - current_entities
         for entity in added_entities:
-            current_set.add(entity)
+            node = new_entity_to_node[entity]
+            current_entity_to_node[entity] = node
             entity.handle.__enter__()
-            new_entity_cb(entity, node)
+            on_added_entity(entity, node)
 
-        removed_entities = current_set - new_set
+        removed_entities = current_entities - new_entities
         for entity in removed_entities:
-            removed_entity_cb(entity)
+            on_removed_entity(entity)
             entity.handle.__exit__(None, None, None)
-            current_set.remove(entity)
+            del current_entity_to_node[entity]
 
-        return added_entities or removed_entities
+        return bool(added_entities or removed_entities)
     
     def _handle_ready_entity(
         self,
@@ -202,31 +186,29 @@ class AsyncioExecutor(BaseExecutor):
         node: Node,
         number_of_events: int,
     ) -> None:
-        if node not in self._node_to_entities:
+        if node not in self._nodes:
             return
-        
-        tasks = self._node_to_entities[node].tasks
+
+        tasks = self._node_to_tasks[node]
         for _ in range(number_of_events):
             callback = take_entity_callback(entity)
             if not callback:
                 return
-            
+
             task = self._loop.create_task(callback)
-            task.add_done_callback(partial(self._done_callback, entity, node))
+            task.add_done_callback(partial(self._done_callback, node))
             tasks.add(task)
 
     def _done_callback(
         self,
-        entity: EntityT,
-        node_tasks: Set[Task],
-        task: Task,
-    ):
+        node: Node,
+        task: asyncio.Task,
+    ) -> None:
         if task.cancelled():
             return
-        
+
+        self._node_to_tasks[node].remove(task)
+
         exc = task.exception()
         if exc:
-            logger = get_logger(entity.get_logger_name())
-            logger.error(''.join(traceback.format_exception(exc)))
-        
-        node_tasks.remove(task)
+            node.get_logger().error(''.join(traceback.format_exception(exc)))
