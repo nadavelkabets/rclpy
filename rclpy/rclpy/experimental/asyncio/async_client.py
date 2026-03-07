201b927004dc2c89c5e9cb6ad4d6b94c1c3b259f
Nadav Elkabets <elnadav12@gmail.com>
2026-03-07 23:32:56 +0000

    Make callback execution sequential per entity by default, export reader to async iterator
    Signed-off-by: Nadav Elkabets <elnadav12@gmail.com>

--- rclpy/rclpy/experimental/asyncio/async_client.py
+++ rclpy/rclpy/experimental/asyncio/async_client.py
@@ -18,6 +18,7 @@ class AsyncClient(BaseClient[SrvRequestT, SrvResponseT]):
     ) -> None:
         super().__init__(client_impl, srv_type, srv_name, qos_profile)
         self._pending_requests: Dict[int, asyncio.Future] = {}
+        self._closing = False
         self._task: Optional[asyncio.Task] = None
         self._loop: Optional[asyncio.AbstractEventLoop] = None
         self._read_event: Optional[asyncio.Event] = None
@@ -55,30 +56,19 @@ class AsyncClient(BaseClient[SrvRequestT, SrvResponseT]):
         finally:
             self._pending_requests.pop(sequence_number, None)
 
-    async def _run(self) -> None:
-        """DDS bridge response loop for clients."""
-        self._loop = asyncio.get_running_loop()
-        self._task = asyncio.current_task()
-        self._read_event = asyncio.Event()
-
+    async def _responses(self):
+        """Async generator yielding (header, response) from DDS."""
         with self.handle:
             self.handle.set_on_new_response_callback(self._on_new_response)
             try:
-                while True:
+                while not self._closing:
                     header_and_response = self.handle.take_response(
                         self.srv_type.Response)
                     if header_and_response != (None, None):
-                        header, response = header_and_response
-                        future = self._pending_requests.get(
-                            header.request_id.sequence_number)
-                        if future is not None:
-                            future.set_result(response)
+                        yield header_and_response
                     else:
-                        try:
-                            self._read_event.clear()
-                            await self._read_event.wait()
-                        except asyncio.CancelledError:
-                            break
+                        self._read_event.clear()
+                        await self._read_event.wait()
             finally:
                 self.handle.clear_on_new_response_callback()
                 for future in self._pending_requests.values():
@@ -86,12 +76,22 @@ class AsyncClient(BaseClient[SrvRequestT, SrvResponseT]):
                 self._pending_requests.clear()
                 self.handle.destroy_when_not_in_use()
 
+    async def _run(self) -> None:
+        """DDS bridge response loop for clients."""
+        self._loop = asyncio.get_running_loop()
+        self._task = asyncio.current_task()
+        self._read_event = asyncio.Event()
+
+        async for header, response in self._responses():
+            future = self._pending_requests.get(
+                header.request_id.sequence_number)
+            if future is not None:
+                future.set_result(response)
+
     async def close(self) -> None:
-        """Cancel the response loop and cancel pending requests."""
+        """Signal the response loop to stop and cancel pending requests."""
         if self._task is not None:
-            self._task.cancel()
-            try:
-                await self._task
-            except asyncio.CancelledError:
-                pass
+            self._closing = True
+            self._read_event.set()
+            await self._task
             self._task = None
--- rclpy/rclpy/experimental/asyncio/async_subscription.py
+++ rclpy/rclpy/experimental/asyncio/async_subscription.py
@@ -18,12 +18,15 @@ class AsyncSubscription(BaseSubscription[MsgT]):
         callback: Callable[..., Coroutine],
         qos_profile: QoSProfile,
         raw: bool = False,
+        concurrent: bool = False,
     ) -> None:
         if not inspect.iscoroutinefunction(callback):
             raise TypeError('AsyncSubscription callback must be an async function')
         super().__init__(subscription_impl, msg_type, topic, qos_profile, raw)
         self._callback = callback
         self._callback_type = self._detect_callback_type(callback)
+        self._concurrent = concurrent
+        self._closing = False
         self._task: Optional[asyncio.Task] = None
         self._loop: Optional[asyncio.AbstractEventLoop] = None
         self._read_event: Optional[asyncio.Event] = None
@@ -37,41 +40,47 @@ class AsyncSubscription(BaseSubscription[MsgT]):
     def callback(self) -> Callable[..., Coroutine]:
         return self._callback
 
+    async def _messages(self):
+        """Async generator yielding (msg, msg_info) from DDS."""
+        with self.handle:
+            self.handle.set_on_new_message_callback(self._on_new_message)
+            try:
+                while not self._closing:
+                    msg_and_info = self.handle.take_message(
+                        self.msg_type, self.raw)
+                    if msg_and_info is not None:
+                        yield msg_and_info
+                    else:
+                        self._read_event.clear()
+                        await self._read_event.wait()
+            finally:
+                self.handle.clear_on_new_message_callback()
+                self.handle.destroy_when_not_in_use()
+
+    def _make_callback(self, msg_and_info: tuple) -> Coroutine:
+        """Create a callback coroutine from a (msg, msg_info) tuple."""
+        if self._callback_type is BaseSubscription.CallbackType.MessageOnly:
+            return self._callback(msg_and_info[0])
+        return self._callback(*msg_and_info)
+
     async def _run(self) -> None:
         """DDS bridge read loop for subscriptions."""
         self._loop = asyncio.get_running_loop()
         self._task = asyncio.current_task()
         self._read_event = asyncio.Event()
 
-        with self.handle:
-            self.handle.set_on_new_message_callback(self._on_new_message)
-            try:
-                async with asyncio.TaskGroup() as tg:
-                    while True:
-                        msg_and_info = self.handle.take_message(
-                            self.msg_type, self.raw)
-                        if msg_and_info is not None:
-                            if self._callback_type is BaseSubscription.CallbackType.MessageOnly:
-                                msg_tuple = (msg_and_info[0],)
-                            else:
-                                msg_tuple = msg_and_info
-                            tg.create_task(self._callback(*msg_tuple))
-                        else:
-                            try:
-                                self._read_event.clear()
-                                await self._read_event.wait()
-                            except asyncio.CancelledError:
-                                break  # let in-flight callbacks complete
-            finally:
-                self.handle.clear_on_new_message_callback()
-                self.handle.destroy_when_not_in_use()
+        if self._concurrent:
+            async with asyncio.TaskGroup() as tg:
+                async for msg_and_info in self._messages():
+                    tg.create_task(self._make_callback(msg_and_info))
+        else:
+            async for msg_and_info in self._messages():
+                await self._make_callback(msg_and_info)
 
     async def close(self) -> None:
-        """Cancel the read loop and wait for in-flight callbacks to complete."""
+        """Signal the read loop to stop and wait for in-flight callbacks."""
         if self._task is not None:
-            self._task.cancel()
-            try:
-                await self._task
-            except asyncio.CancelledError:
-                pass
+            self._closing = True
+            self._read_event.set()
+            await self._task
             self._task = None
